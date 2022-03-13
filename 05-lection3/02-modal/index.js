aa77052f294c93c11b3a6064e5a95bd4c61de1ab
John Hopkins <cov12ssd@gmail.com>
2022-03-13 19:07:44 +0300

    end

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -2,18 +2,21 @@
     const close = document.getElementById('modal-close');
     const modal = document.getElementById('modal');
     const modalOpen = document.getElementById('modal-open');
+    const body = document.getElementsByTagName('body');
 
     if (!close || !modalOpen || !modal) {
         return;
     }
 
     modalOpen.addEventListener('click', () => {
-        modal.style.display = 'block';
+        modal.style.display = 'flex';
+        body[0].classList.add('body__modal');
         document.body.overflow = 'hidden';
     });
 
     close.addEventListener('click', () => {
         modal.style.display = 'none';
+        body[0].classList.remove('body__modal');
         document.body.overflow = 'initial';
     });
 })();
